7e5a743356adae316b73ac29ab726a44993c10b2
Ramarao <ramaraod711@gmail.com>
2020-07-26 21:35:22 +0530

    Added new java code into development

--- src/collections/HelloJava.java
+++ src/collections/HelloJava.java
@@ -5,6 +5,11 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Java i am ramarao");
+System.out.println("Hello Derangulla family");
+System.out.println("Hello Bhanu");
+System.out.println("Hello Ram");
+System.out.println("Hello Srinu");
+System.out.println("Hello Kashimma");
 	}
 
 }
