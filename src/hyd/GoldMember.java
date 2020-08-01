38343dae5a07c2635e8a95a591bff74b223f219d
Ramarao <ramaraod711@gmail.com>
2020-08-01 18:20:28 +0530

    Gold class added

---
+++ src/hyd/GoldMember.java
@@ -0,0 +1,11 @@
+package hyd;
+
+public class GoldMember {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello i am gold member");
+System.out.println("Hello i am platinum");
+	}
+
+}
